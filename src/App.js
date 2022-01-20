286e15abbe47023e253e5b350ce2e546253703c6
Fernando Henrique <cara-se@hotmail.com>
2022-01-20 20:59:59 -0300

    Projeto concluido

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { FiSearch } from "react-icons/fi";
+import HeadShake from "react-reveal/HeadShake";
+import api from "./services/api";
 
 function App() {
+    const [input, setInput] = useState('');
+    const [github, setGithub] = useState({});
+    async function handleSearch() {
+        if (input === "") {
+          alert("Preencha o Input com seu Usuario")
+        }
+
+        try {
+            const response = await api.get(`${input}`);
+            setGithub(response.data)
+            setInput('')
+        } catch { 
+          alert("Ops, algo ocorreu. Tente novamente")
+          setInput('')
+
+        }
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">
+        <HeadShake>Github Usuarios</HeadShake>
+      </h1>
+      <div className="inputgroup">
+        <input type="text" placeholder="Digite seu usuario.." value={input} onChange={(event) => setInput(event.target.value)} ></input>
+
+        <button className="buttonSearch">
+          <FiSearch size={20} color="#FFF" onClick={handleSearch} />
+        </button>
+      </div>
+
+      <div className="containerprofile">
+        <img src={github.avatar_url}></img>
+      </div>
+
+      <div className="containergithub">
+        <span>Nome: {github.name}</span>
+        <span>Cidade: {github.location}</span>
+        <span>Username: {github.login}</span>
+        <span>Id: {github.id}</span>
+        <span>Repositorios: {github.public_repos}</span>
+      </div>
     </div>
   );
 }
